999a0cd28a0ad442ec6685900d41c54b603e7c89
Tim Müller <tim@macTim.local>
2019-02-22 18:51:08 +0100

    Added the Factory Plot window() and the Yearly and Daily money plots

--- World.py
+++ World.py
@@ -12,6 +12,11 @@ import threading
 import time
 import inspect
 from collections import defaultdict
+# Do the plot imports
+import matplotlib
+matplotlib.use('TkAgg')
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 import Tools
 from Tools import Market
@@ -20,6 +25,7 @@ from Tools import Command
 import Factory
 from Factory import Factory
 import Modules
+import resources.code.Plots as Plots
 
 def is_int(var):
     try:
@@ -44,7 +50,6 @@ def split_commandline(text):
             else:
                 args[-1] += c
     return command,args
-
 
 class WorldUpdater (threading.Thread):
     def __init__(self, window):
@@ -76,6 +81,7 @@ class WorldUpdater (threading.Thread):
 
         # Init the prev dict
         self.prev = defaultdict(str)
+        self.prev["Plots"] = defaultdict(str)
 
         print("Initialized WorldUpdater thread")
 
@@ -133,16 +139,26 @@ class WorldUpdater (threading.Thread):
             tasks_hash = hash(tuple(self.tasks))
             if self.prev['task_hash'] != tasks_hash:
                 self.prev['task_hash'] = tasks_hash
+                selected = self.window.FactorySelectorVar.get()
                 menu = self.window.FactorySelector["menu"]
                 menu.delete(0, tk.END)
                 for task in self.tasks:
                     if isinstance(task, Factory):
                         menu.add_command(label=task.name, command=lambda value=task.name: self.window.FactorySelectorVar.set(value))
-            
+                # Restore the selected
+                self.window.FactorySelectorVar.set(selected)
+
+            factory_selected = False
             for task in self.tasks:
                 if isinstance(task, Factory) and task.name == self.window.FactorySelectorVar.get():
                     self.update_factory(task)
+                    factory_selected = True
                     break
+            # If we have a factory, set the buttons to enable
+            if factory_selected:
+                self.window.btnPlots.config(state="normal")
+            else:
+                self.window.btnPlots.config(state="disabled")
 
             # Check for unfreezing interval
             if self.freeze_duration > -1 and time.time() - self.freeze_start > self.freeze_duration:
@@ -225,6 +241,53 @@ class WorldUpdater (threading.Thread):
             self.prev['workers_info'] = info
             self.window.lblWorkerInfo.config(text=info)
 
+        # Also update the plots
+        if self.window.plot_window != None:
+            self.update_plots(factory)
+            if not tk.Toplevel.winfo_exists(self.window.plot_window.root):
+                self.window.plot_window = None
+
+    # Updates the Plot window 
+    def update_plots (self, factory):
+        # Get the to-be-updated plot
+        plot = None
+        for plt in self.window.plot_window.plots:
+            if plt.name == self.window.plot_window.lstPlotSelectorVar.get():
+                plot = plt
+                break
+        if plot == None: return
+        # Set the title to that plot
+        if "{factory}" in plot.title:
+            plot.title = plot.title.replace("{factory}", factory.name)
+
+        # Collect the info for the plot from the archive
+        values = {}
+        if plot.name == "Yearly Balance":
+            # Get the years (only last hundred years)
+            found_year = factory.modules.archive.get("General", "Founded").year
+            newest_year = factory.time.year
+            oldest_year = newest_year - 100
+            if oldest_year < found_year:
+                oldest_year = found_year
+            values['x_values'] = range(oldest_year, newest_year)
+            values['y_values'] = factory.modules.archive.get("Finance", "Yearly Balances")[-100:]
+        elif plot.name == "Daily Balance":
+            # Get the last 100 days
+            balances = factory.modules.archive.get("Finance", "Daily Balances")[-100:]
+            # Get the dates
+            newest_date = factory.time.now() - Tools.Date(0, 1, 0, 0)
+            oldest_date = newest_date - Tools.Date(0, len(balances), 0, 0)
+            
+            values['x_values'] = [(newest_date - Tools.Date(0, day, 0, 0)).getdate() for day in range((newest_date - oldest_date).todays())]
+            values['y_values'] = range(len(balances))
+            values['y_ticks'] = balances
+        # Check if we ACTUALLY need to update
+        values_hash = hash(frozenset(values.items()))
+        if values_hash != self.prev["Plots"][plot.name]:
+            self.prev["Plots"][plot.name] = values_hash
+            # Prepare to set the update
+            self.window.plot_window.schedule_plot_update(plot.plot, self.window.plot_window.a, values)
+
     # Prints on the command
     def print(self, text, end="\n"):
         if type(text) != str:
@@ -293,11 +356,13 @@ class WorldUpdater (threading.Thread):
         updater.print("**************************")
     def command_quit (self, updater, _):
         updater.print("Stopping...\n")
-        updater.window.root.destroy()
+        updater.window.destroy()
         updater.running = False
 
 class Window ():
     def __init__(self):
+        self.plot_window = None
+
         self.stopped = False
 
         print("Loading interface...")
@@ -368,8 +433,17 @@ class Window ():
         self.lblFactoryInfo = tk.Label(self.boxFactoryInfo, text="", justify=tk.LEFT)
         self.lblFactoryInfo.pack(side=tk.LEFT)
 
+        self.boxAdditional = tk.LabelFrame(self.tab_factory, text="Additional Info")
+        self.boxAdditional.grid(padx=10,pady=10,row=1,column=2,sticky=tk.N+tk.W+tk.E+tk.S)
+
+        self.btnPlots = tk.Button(self.boxAdditional, text="Factory Plots", command=self.showPlots)
+        self.btnPlots.pack()
+
+        self.btnLayout = tk.Button(self.boxAdditional, text="Factory Layout", state=tk.DISABLED)
+        self.btnLayout.pack()
+
         self.boxModuleInfo = tk.LabelFrame(self.tab_factory,text="Modules")
-        self.boxModuleInfo.grid(padx=10,pady=10,row=2,column=0,columnspan=2,sticky=tk.N+tk.E+tk.S+tk.W)
+        self.boxModuleInfo.grid(padx=10,pady=10,row=2,column=0,columnspan=3,sticky=tk.N+tk.E+tk.S+tk.W)
 
         self.list_modules = tk.Listbox(self.boxModuleInfo, exportselection=False)
         self.list_modules.grid(padx=10,pady=10,row=0,column=0,sticky=tk.N+tk.S+tk.W+tk.E)
@@ -411,6 +485,14 @@ class Window ():
         self.root.bind("<Return>", self.buttonClick)
 
         print("Done")
+
+    def destroy (self):
+        # Destroy child windows (if they're there)
+        if self.plot_window != None:
+            self.plot_window.root.destroy()
+
+        # Destroy self
+        self.root.destroy()
     
     def buttonClick(self, other=None):
         # Parse the command
@@ -420,6 +502,62 @@ class Window ():
         # Run them
         self.updater.handle_command(command, args)
         
+    def showPlots(self, other=None):
+        window = tk.Toplevel(self.root)
+        self.plot_window = PlotWindow(window)
+
+class PlotWindow ():
+    plots = [
+        Plots.Plot("Yearly Balance", "plot", "Yearly Balances of {factory}", "Year", "Balance"),
+        Plots.Plot("Daily Balance", "plot", "Daily Balances of {factory}", "Date", "Balance")
+    ]
+
+    def __init__(self, root):
+        # Prepare the plot update task list
+        self.plot_tasks = []
+
+        self.root = root
+
+        # Add the listbox
+        self.lstPlotSelectorVar = tk.StringVar(self.root)
+        self.lstPlotSelector = tk.OptionMenu(self.root, self.lstPlotSelectorVar, *[plot.name for plot in self.plots])
+        self.lstPlotSelector.grid(padx=10,pady=(10),column=0,row=0,sticky=tk.E+tk.W)
+        self.lstPlotSelectorVar.set(self.plots[0].name)
+
+        # Add the plot frame
+        self.boxPlot = tk.LabelFrame(self.root, text="Plot")
+        self.boxPlot.grid(padx=10,pady=(0,10),column=0,row=1,sticky=tk.N+tk.E+tk.S+tk.W)
+
+        # Create the figure
+        self.fig = Figure()
+        self.a = self.fig.add_subplot(111)
+
+        # Create the plot drawer
+        self.plot = FigureCanvasTkAgg(self.fig, master=self.boxPlot)
+        self.plot.get_tk_widget().pack(expand=True, fill="both")#grid(padx=10,pady=10,column=0,row=1,sticky=tk.N+tk.E+tk.S+tk.W)
+        self.plot.draw()
+
+        # Do some row configures
+        self.root.rowconfigure(1, weight=1)
+        self.root.columnconfigure(0, weight=1)
+
+        # Begin the update loop
+        self.update_plot()
+    
+    def update_plot(self):
+        if len(self.plot_tasks) > 0:
+            # Run the first task
+            plot_func, sub_plot, values = self.plot_tasks[0]
+            plot_func(sub_plot, values)
+            # Draw
+            self.plot.draw()
+            self.plot_tasks = self.plot_tasks[1:]
+        # Call again after .5 seconds
+        self.boxPlot.after(500, self.update_plot)
+    
+    def schedule_plot_update(self, plot_func, sub_plot, values):
+        self.plot_tasks.append((plot_func, sub_plot, values))
+
 def main ():
     window = Window()
 
