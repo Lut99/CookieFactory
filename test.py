d1da76e38a270f2f6fc8e1409ecaf42f77e27783
Tim Müller <tim@macTim.local>
2019-02-22 23:52:43 +0100

    Some testing on Date

--- test.py
+++ test.py
@@ -2,9 +2,12 @@ from Tools import Date
 import time
 
 d = Date(0, 0, 0, 0)
-print(d)
+print(d.epoch)
 
+ticked = []
 for i in range(50):
+    hour, day, month, year = Date.depochify(d.epoch)
+    epoch = Date.epochify(hour, day, month, year)
+    print(str(d) + " (ticked: [" + ', '.join(ticked) + "], epoch - epochify(depochify): " + str(epoch - d.epoch) + ")")
     ticked = d.tick(24)
-    print(str(d) + " (ticked: " + ', '.join(ticked) + ")")
     time.sleep(.1)
