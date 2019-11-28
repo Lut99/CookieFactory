2bd6af14b805561b1ca6c5d7d67beed232545fdf
Lut99 <mullertim99@gmail.com>
2019-11-28 12:06:51 +0100

    Worked a test for void pointer casting

---
+++ lib/TestSuite/src/test.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+template <typename T> void *to_pointer(T elem) {
+    T *p = new T;
+    (*p) = elem;
+    return static_cast<void*>(p);
+}
+template <typename T> T from_pointer(void *p) {
+    // Cast and return
+    return (*(static_cast<T*>(p)));
+}
+
+int main() {
+    string test = "Hello, there!";
+    void *p = to_pointer(test);
+    // Print the returned value
+    cout << from_pointer<string>(p) << endl;
+
+    // Delete
+    delete p;
+
+    return 0;
+}
